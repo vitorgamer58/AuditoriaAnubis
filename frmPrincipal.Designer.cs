773d23971fd0955520918d27f4c198ef3d5fb0cc
MatheusGrijo <matheusgrijo@matheusgrijo.com.br>
2019-09-05 13:25:55 -0300

    initial

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace AuditoriaAnubisTrades
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApiAnubisTrade = new System.Windows.Forms.TextBox();
+            this.txtApiBinance = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "API AnubisTrade:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Api Binance:";
+            // 
+            // txtApiAnubisTrade
+            // 
+            this.txtApiAnubisTrade.Location = new System.Drawing.Point(102, 16);
+            this.txtApiAnubisTrade.Name = "txtApiAnubisTrade";
+            this.txtApiAnubisTrade.Size = new System.Drawing.Size(570, 20);
+            this.txtApiAnubisTrade.TabIndex = 2;
+            this.txtApiAnubisTrade.Text = "https://anubis.website/api/binance/allOrders/?symbol=BTCUSDT";
+            // 
+            // txtApiBinance
+            // 
+            this.txtApiBinance.Location = new System.Drawing.Point(102, 47);
+            this.txtApiBinance.Name = "txtApiBinance";
+            this.txtApiBinance.Size = new System.Drawing.Size(570, 20);
+            this.txtApiBinance.TabIndex = 3;
+            this.txtApiBinance.Text = "https://api.binance.com/api/v1/aggTrades?symbol=BTCUSDT&startTime=@start&endTime=" +
+    "@end";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 284);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 434);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(763, 82);
+            this.listBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-1, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Trades realizados:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Logs:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(378, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Volume BTC negociados na Anubis no período:";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVolume.Location = new System.Drawing.Point(612, 81);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(102, 13);
+            this.lblVolume.TabIndex = 9;
+            this.lblVolume.Text = "0.00000000 BTC";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(676, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 55);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Começar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Start time:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(186, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "End Time:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(91, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(675, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "*Número de trades é diferente do número de ordens, dependendo da liquidez do merc" +
+    "ado na Binance | Sempre utilizar o UTC-0 para as datas!";
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(68, 77);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(112, 20);
+            this.txtStart.TabIndex = 14;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(243, 77);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(112, 20);
+            this.txtEnd.TabIndex = 15;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(98, 114);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(13, 13);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "0";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 521);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtApiBinance);
+            this.Controls.Add(this.txtApiAnubisTrade);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auditoria Anubis - Trades";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApiAnubisTrade;
+        private System.Windows.Forms.TextBox txtApiBinance;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuditoriaAnubisTrades
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string getCache(String url, bool wait = true)
+        {
+            try
+            {
+
+
+
+                String r = "";
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                httpWebRequest.Method = "GET";
+                var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                var responseStream = httpWebResponse.GetResponseStream();
+                if (responseStream != null)
+                {
+                    var streamReader = new StreamReader(responseStream);
+                    r = streamReader.ReadToEnd();
+                }
+                if (responseStream != null) responseStream.Close();
+
+
+
+
+                return r;
+            }
+            catch (WebException ex)
+            {
+                return null;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            run();
+        }
+
+
+        private void log(string value)
+        {
+            listBox1.Items.Add("[" + DateTime.Now.ToString() + "] - " + value);
+            this.listBox1.SelectedIndex = this.listBox1.Items.Count - 1;
+            this.Show();
+            this.Refresh();
+        }
+
+        private void run()
+        {
+
+            listBox1.Items.Clear();
+            dataGridView1.Rows.Clear();
+
+
+            double volume = 0;
+            int total = 0;
+
+            button1.Enabled = false;
+            log("Start Auditoria...");
+            //Pegando dados da Binance de 1h em 1h limite operacional da  api da mesma
+            log("Get API Anubis...");
+            String jsonAnubis = getCache(txtApiAnubisTrade.Text);
+            Newtonsoft.Json.Linq.JContainer jAnubis = (Newtonsoft.Json.Linq.JContainer)JsonConvert.DeserializeObject(jsonAnubis);
+
+            System.Data.DataTable dt = new DataTable();
+            dt.Columns.Add("id");
+            dt.Columns.Add("orderId");
+            dt.Columns.Add("price");
+            dt.Columns.Add("volume");
+            dt.Columns.Add("timestamp");
+            dt.Columns.Add("date");
+            dt.Columns.Add("isbuyer");
+            dt.Columns.Add("commission");
+            dt.Columns.Add("commissionAsset");
+
+            TimeSpan ts = DateTime.Parse(txtEnd.Text) - DateTime.Parse(txtStart.Text);
+            for (int i = 0; i < Math.Round(ts.TotalHours); i++)
+            {
+
+                
+
+                double start = 0;
+                double end = 0;
+                start = DatetimeToUnix(DateTime.Parse(txtStart.Text).AddHours(i));
+                end = DatetimeToUnix(DateTime.Parse(txtStart.Text).AddHours(i + 1));
+                log("Get Binance item " + i.ToString() + " of " + Math.Round(ts.TotalHours) + " (" + UnixTimeStampToDateTime( start) + " at " + UnixTimeStampToDateTime(end )+ ") ...");
+
+                if (start > DatetimeToUnix(DateTime.UtcNow))
+                    break;
+
+                String jsonBinance = getCache(txtApiBinance.Text.Replace("@start", start.ToString()).Replace("@end", end.ToString()));
+                Newtonsoft.Json.Linq.JContainer jBinance = (Newtonsoft.Json.Linq.JContainer)JsonConvert.DeserializeObject(jsonBinance);
+
+
+                log("Verify trades...");
+                foreach (var itemAnubis in jAnubis)
+                {
+                    if (long.Parse(itemAnubis["time"].ToString()) >= start && long.Parse(itemAnubis["time"].ToString()) <= end)
+                    {
+                        foreach (var item in jBinance)
+                        {
+                            if ((long.Parse(itemAnubis["id"].ToString()) >= long.Parse(item["f"].ToString()) &&
+                                long.Parse(itemAnubis["id"].ToString()) <= long.Parse(item["l"].ToString()))
+
+                                )
+                            {
+                                dt.Rows.Add(itemAnubis["id"].ToString(), itemAnubis["orderId"].ToString(), item["p"], itemAnubis["qty"], itemAnubis["time"], UnixTimeStampToDateTime(double.Parse(itemAnubis["time"].ToString())), itemAnubis["isBuyer"],item["commission"],item["commissionAsset"]);
+                                dataGridView1.DataSource = dt;
+                                
+                                total++;
+                                volume += double.Parse(itemAnubis["qty"].ToString().Replace(".",","));
+
+                                lblTotal.Text = total.ToString();
+                                lblVolume.Text = volume.ToString() + " BTC";
+
+                                log("Trade "+ itemAnubis["id"].ToString() + " found! OrderId: " + itemAnubis["orderId"].ToString());
+                            }
+                        }
+                    }
+                }
+
+                log("Wait 1s...");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            log("end!");
+            button1.Enabled = true;
+            MessageBox.Show("End!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtStart.Text = DateTime.UtcNow.ToString("yyyy-MM-dd") + " 00:00:00";
+            txtEnd.Text = DateTime.UtcNow.ToString("yyyy-MM-dd") + " 23:59:59";
+        }
+
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(unixTimeStamp).ToUniversalTime();
+            return dtDateTime;
+        }
+
+        public static Double DatetimeToUnix(DateTime date)
+        {
+            return (date.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds;
+        }
+    }
+}
