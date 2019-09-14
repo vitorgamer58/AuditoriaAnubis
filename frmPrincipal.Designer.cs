1bb8c5a3f61bbc8e2bf4d0b4ba456eb572ee719e
vitorgamer58 <vitorgamer58@outlook.com>
2019-09-14 15:36:41 -0300

    Logo adicionado

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtApiAnubisTrade = new System.Windows.Forms.TextBox();
@@ -219,6 +220,7 @@
             this.Controls.Add(this.txtApiAnubisTrade);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auditoria Anubis - Trades";
